a864e451f6957c5b1664af05d36cb2ad5415d411
Robert Russell <23439640+WarpRat@users.noreply.github.com>
2018-05-01 15:20:32 -0700

    Fixed globbing error and added cleanup

--- nfs-client.sh
+++ nfs-client.sh
@@ -15,7 +15,7 @@ apt-get install -y nfs-client
 #Provided that the client was able to talk to the server, create mount points and add them to fstab
 if [ -s /tmp/avail_mounts ]; then
 	while read line; do
-		dir=$(echo $line | sed 's/.*[^/]\/\(.*\).*\*/\1/')
+		dir=$(echo "$line" | sed 's/.*[^/]\/\(.*\).*\*/\1/')
 		mkdir -p /mnt/$dir
 		echo "$NFS_SERVER:$(echo $line | cut -d ' ' -f 1)    /mnt/$dir    nfs    defaults 0 0" >> /etc/fstab
 	done < /tmp/avail_mounts
@@ -23,3 +23,6 @@ if [ -s /tmp/avail_mounts ]; then
 else
 	echo "No NFS server found, or some other error. Sorry!" >> /root/showmounterr.log
 fi
+
+#Cleanup debugging file if it wasn't needed
+[ -s /root/showmounterr.log ] || rm /root/showmounterr.log
