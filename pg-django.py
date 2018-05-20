48a25d7602acd790fbafe4caf9efdddfe5f5970b
Robert Russell <23439640+WarpRat@users.noreply.github.com>
2018-05-20 15:57:14 -0700

    Added password creation

--- pg-django.py
+++ pg-django.py
@@ -15,25 +15,28 @@ import os
 import time
 from pprint import pprint
 import re
+import random
+import string
 
 project = 'nti310-320'
 zone = 'us-west1-a'
 name = 'test-instance-100'
-compute = googleapiclient.discovery.build('compute', 'v1')
+pw_dir = '.script_passwd'
+#compute = googleapiclient.discovery.build('compute', 'v1')
 
 
 def create_instance(compute, name, startup_script, project, zone):
   image_response = compute.images().getFromFamily(
     project='centos-cloud', family='centos-7').execute()
   source_disk_image = image_response['selfLink']
-  
+
   machine_type = 'zones/%s/machineTypes/f1-micro' % zone
 
-  
+
   config = {
   	'name': name,
   	'machineType': machine_type,
-  	
+
   	'disks': [
   	  {
   	  	'boot': True,
@@ -44,14 +47,14 @@ def create_instance(compute, name, startup_script, project, zone):
   	  	}
   	  }
   	],
-  	
+
   	'networkInterfaces': [{
   		'network': 'global/networks/default',
   		'accessConfigs': [
   		  {'type': 'ONE_TO_ONE_NAT', 'name': 'External NAT', 'networkTier': 'PREMIUM'}
   		  ]
   		}],
-  
+
     'description': '',
     'labels': {},
     'scheduling': {
@@ -85,7 +88,7 @@ def create_instance(compute, name, startup_script, project, zone):
        }]
     }
   }
-  
+
   return compute.instances().insert(
     project=project,
     zone=zone,
@@ -111,25 +114,25 @@ def wait_for_operation(compute, project, zone, operation):
 
 
 
-def build(name, startup_script):  
-  
+def build(name, startup_script):
+
   operation = ''
   result = ''
-  
+
   try:
     operation = create_instance(compute, name, startup_script, project, zone)
     #pprint(operation)
     wait_for_operation(compute, project, zone, operation['name'])
-    
-    
+
+
   except Exception as e:
-    print('ERRROR')
+    print('ERROR')
     print(e)
     if name in str(e) and 'already exists' in str(e):
       if re.search(r'-[0-9]', name[-2:]):
         name = name[:-1] + str(int(name[-1:]) + 1)
         return build(name, startup_script)
-        
+
       else:
         name = name + '-1'
         return build(name, startup_script)
@@ -138,22 +141,53 @@ def build(name, startup_script):
      return operation['targetId']
 
 def postgres():
-    
+
     startup_script = open(
     os.path.join(
       os.path.dirname(__file__), 'pgsql-install.sh'), 'r').read()
     startup_script_edit = re.sub(r'(?<=postgres WITH PASSWORD ).*;', '\'{postgres}\';', startup_script)
-    startup_script_edit = re.sub(r'(?<=db_srv WITH PASSWORD ).*;', '{db_srv};', startup_script_edit)
-    
-    startup_script = startup_script_edit.format(postgres='test123', db_srv='test321')
-    
+    startup_script_edit = re.sub(r'(?<=db_srv WITH PASSWORD ).*;', '\'{db_srv}\';', startup_script_edit)
+    pg_pw = pw_gen(24)
+    db_srv = pw_gen(24)
+    startup_script = startup_script_edit.format(postgres=pg_pw, db_srv=db_srv)
+
     db_id = build('postgres', startup_script)
     filter_id = 'id=' + db_id
     result = compute.instances().list(project=project, zone=zone, filter=filter_id).execute()
+
+    pg_pw_file = result['items'][0]['id'] + '-postgres'
+    db_srv_file = result['items'][0]['id'] + '-db_srv'
+    save_pw(pg_pw, pg_pw_file)
+    save_pw(pg_pw, pg_pw_file)
+
     return result['items'][0]['networkInterfaces'][0]['accessConfigs'][0]['natIP']
 
-    
+def pw_gen(length):
+    char_gen = random.SystemRandom()
+    char_map = string.ascii_letters + string.digits
+    return ''.join([ char_gen.choice(char_map) for _ in xrange(length) ])
+
+def save_pw(new_pass, name):
+    '''Make sure that a directory exists and write the password to a file
+    with restrictive permissions for human use.'''
+    user_home = os.path.expanduser('~'+os.environ['LOGNAME']+'/')
+    if not os.path.isdir(
+      os.path.join(os.path.dirname(__file__), pw_dir)):
+      print('making directory')
+      os.makedirs(pw_dir, 0700)
+    else:
+      print('exists')
+
+    os.umask(0)
+    with os.fdopen(os.open(os.path.join(os.path.dirname(__file__), pw_dir, name), os.O_WRONLY | os.O_CREAT, 0o600), 'w') as pw_file:
+        pw_file.write(new_pass)
+
+
 if __name__ == '__main__':
-  
-  
+
+    user_home = os.path.expanduser('~'+os.environ['LOGNAME']+'/')
+    print(user_home)
+    #test_pass = pw_gen(32)
+    #save_pw(test_pass, 'nadda')
+
   #build(name, 'nfs-server.sh')
